ccc3027ed21cfae7d61da036467374dcc8c50953
Anton Agafonnikov <feograf@mail.ru>
2023-07-29 21:29:02 +0300

    Correction of remarks for branch "webapp-runner"

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -2,29 +2,45 @@ package ru.netology.repository;
 
 import ru.netology.model.Post;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
 public class PostRepository {
-  private final ConcurrentMap<Long, Post> posts = new ConcurrentHashMap<Long, Post>();
-  public ConcurrentMap<Long, Post> all() {
-    return posts;
+  private final ConcurrentMap<Long, Post> posts = new ConcurrentHashMap<>();
+  private static final AtomicLong countPostID = new AtomicLong(0);
+  public List<Post> all() {
+    return new ArrayList<>(posts.values());
   }
 
   public Optional<Post> getById(long id) {
-    return Optional.ofNullable(posts.get(id));
+    if(posts.containsKey(id)) {
+      return Optional.ofNullable(posts.get(id));
+    } else {
+      return Optional.empty();
+    }
   }
 
-  public Post save(Long id, Post post) {
-    posts.put(id, post);
+  public Post save(Post post) {
+    if(posts.containsKey(post.getId())) {
+      posts.put(post.getId(), post);
+    } else {
+      post.setId(countPostID.incrementAndGet());
+      posts.put(countPostID.get(), post);
+    }
     return post;
   }
 
-  public void removeById(long id) {
-    posts.remove(id);
+  public boolean removeById(long id) {
+    if(posts.containsKey(id)) {
+      posts.remove(id);
+      return true;
+    } else {
+      return false;
+    }
   }
 }
