69b8e84b33133c5fbcece98c5a9d56e48222c857
Anton Agafonnikov <feograf@mail.ru>
2023-07-30 23:54:00 +0300

    Add annotation config

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.model.Post;
 
 import java.util.ArrayList;
@@ -9,7 +10,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicLong;
 
-// Stub
+@Repository
 public class PostRepository {
   private final ConcurrentMap<Long, Post> posts = new ConcurrentHashMap<>();
   private static final AtomicLong countPostID = new AtomicLong(0);
